d69631436bb76fba40ce419997ba48b70efb9ba4
Hadi <106028128+Hadi-Baydoun@users.noreply.github.com>
2023-07-20 10:00:26 +0300

    Add files via upload

---
+++ Designo multi-page/script.js
@@ -0,0 +1,10 @@
+const menuToggle = document.getElementById('menu-toggle');
+const menu = document.querySelector('.menu');
+
+menuToggle.addEventListener('change', function() {
+    if (menuToggle.checked) {
+        menu.style.display = 'flex';
+    } else {
+        menu.style.display = 'none';
+    }
+});
